263fa0f27b782d04f5ee689f43d2b466c4ee3a41
James Hartt <jameshartt@gmail.com>
2017-08-24 10:13:36 +0100

    Add video

--- index.js
+++ index.js
@@ -11,21 +11,18 @@ import {
   Text,
   View
 } from 'react-native';
+import { Video } from 'react-native-video'
 
 export default class Future extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Video
+          repeat
+          resizeMode='cover'
+          source={require('./assets/video/turntable.mp4')}
+          style={styles.backgroundVideo}
+          />
       </View>
     );
   }
