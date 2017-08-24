55b194840c8a79573bd776333341a39cacce4ab9
James Hartt <jameshartt@gmail.com>
2017-08-24 11:01:18 +0100

    Working on iOS and Android

--- index.ios.js
+++ index.ios.js
@@ -37,10 +37,10 @@ const styles = StyleSheet.create({
   },
   backgroundVideo: {
     position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
 
